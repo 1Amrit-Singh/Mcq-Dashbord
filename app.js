fecc63142bfbeb461e84220f418106d8c6b8af39
Amrit Pritam Sangramsingh <90082064+1Amrit-Singh@users.noreply.github.com>
2024-02-17 18:57:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 /* Created and coded by Amrit Pritam Sangramsingh */
 /* Quiz source: w3schools.com */
 // Add these variables at the beginning of your app.js file
-var totalTimeInSeconds = 1800; // Set the total time in seconds (e.g., 5 minutes)
+var totalTimeInSeconds = 30; // Set the total time in seconds (e.g., 5 minutes)
 var timeLeft = totalTimeInSeconds;
 var countdownInterval;
 
@@ -287,23 +287,23 @@ var quizApp = function () {
 this.score = 0;
 this.qno = 1;
 this.currentque = 0;
-var totalque = quiz.JS.length;
+var totalque = quiz.Python.length;
 this.displayQuiz = function (cque) {
 this.currentque = cque;
 if (this.currentque < totalque) {
 $("#tque").html(totalque);
 $("#previous").attr("disabled", false);
 $("#next").attr("disabled", false);
-$("#qid").html(quiz.JS[this.currentque].id + '.');
-$("#question").html(quiz.JS[this.currentque].question);
+$("#qid").html(quiz.Python[this.currentque].id + '.');
+$("#question").html(quiz.Python[this.currentque].question);
 $("#question-options").html("");
-for (var key in quiz.JS[this.currentque].options[0]) {
-if (quiz.JS[this.currentque].options[0].hasOwnProperty(key)) {
+for (var key in quiz.Python[this.currentque].options[0]) {
+if (quiz.Python[this.currentque].options[0].hasOwnProperty(key)) {
 $("#question-options").append(
 "<div class='form-check option-block'>" +
 "<label class='form-check-label'>" +
-"<input type='radio' class='form-check-input' name='option' id='q" + key + "' value='" + quiz.JS[this.currentque].options[0][key] + "'><span id='optionval'>" +
-quiz.JS[this.currentque].options[0][key] +
+"<input type='radio' class='form-check-input' name='option' id='q" + key + "' value='" + quiz.Python[this.currentque].options[0][key] + "'><span id='optionval'>" +
+quiz.Python[this.currentque].options[0][key] +
 "</span></label>"
 );
 }
@@ -315,7 +315,7 @@ $("#previous").attr("disabled", true);
 if (this.currentque >= totalque) {
 $('#next').attr('disabled', true);
 for (var i = 0; i < totalque; i++) {
-this.score = this.score + quiz.JS[i].score;
+this.score = this.score + quiz.Python[i].score;
 }
 return this.showResult(this.score);
 }
@@ -325,31 +325,31 @@ $("#result").addClass('result');
 $("#result").html("<h1 class='res-header'>Total Score: &nbsp;" + scr + '/' + totalque + "</h1>");
 for (var j = 0; j < totalque; j++) {
 var res;
-if (quiz.JS[j].score == 0) {
-res = '<span class="wrong">' + quiz.JS[j].score + '</span><i class="fa fa-remove c-wrong"></i>';
+if (quiz.Python[j].score == 0) {
+res = '<span class="wrong">' + quiz.Python[j].score + '</span><i class="fa fa-remove c-wrong"></i>';
 } else {
-res = '<span class="correct">' + quiz.JS[j].score + '</span><i class="fa fa-check c-correct"></i>';
+res = '<span class="correct">' + quiz.Python[j].score + '</span><i class="fa fa-check c-correct"></i>';
 }
 $("#result").append(
-'<div class="result-question"><span>Q ' + quiz.JS[j].id + '</span> &nbsp;' + quiz.JS[j].question + '</div>' +
-'<div><b>Correct answer:</b> &nbsp;' + quiz.JS[j].answer + '</div>' +
+'<div class="result-question"><span>Q ' + quiz.Python[j].id + '</span> &nbsp;' + quiz.Python[j].question + '</div>' +
+'<div><b>Correct answer:</b> &nbsp;' + quiz.Python[j].answer + '</div>' +
 '<div class="last-row"><b>Score:</b> &nbsp;' + res +
 '</div>'
 );
 }
 }
 this.checkAnswer = function (option) {
-var answer = quiz.JS[this.currentque].answer;
+var answer = quiz.Python[this.currentque].answer;
 option = option.replace(/</g, "&lt;") //for <
 option = option.replace(/>/g, "&gt;") //for >
 option = option.replace(/"/g, "&quot;")
-if (option == quiz.JS[this.currentque].answer) {
-if (quiz.JS[this.currentque].score == "") {
-quiz.JS[this.currentque].score = 1;
-quiz.JS[this.currentque].status = "correct";
+if (option == quiz.Python[this.currentque].answer) {
+if (quiz.Python[this.currentque].score == "") {
+quiz.Python[this.currentque].score = 1;
+quiz.Python[this.currentque].status = "correct";
 }
 } else {
-quiz.JS[this.currentque].status = "wrong";
+quiz.Python[this.currentque].status = "wrong";
 }
 }
 this.changeQuestion = function (cque) {
